977b7545034b2063755bddc1ec65be83cd49bc54
Freddie Witherden <freddie@witherden.org>
2015-07-24 13:29:34 +0100

    Switch from FakePointer to DeviceAllocation.

--- pymic/_misc.py
+++ pymic/_misc.py
@@ -1,31 +1,31 @@
-# Copyright (c) 2014-2015, Intel Corporation All rights reserved. 
-# 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are 
-# met: 
-# 
-# 1. Redistributions of source code must retain the above copyright 
-# notice, this list of conditions and the following disclaimer. 
+# Copyright (c) 2014-2015, Intel Corporation All rights reserved.
 #
-# 2. Redistributions in binary form must reproduce the above copyright 
-# notice, this list of conditions and the following disclaimer in the 
-# documentation and/or other materials provided with the distribution. 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
 #
-# 3. Neither the name of the copyright holder nor the names of its 
-# contributors may be used to endorse or promote products derived from 
-# this software without specific prior written permission. 
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
-# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
-# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import print_function
 
@@ -44,7 +44,7 @@ class pymicConfig:
             self._debug_level = int(_debug_level)
         except:
             pass
-            
+
         # PYMIC_TRACE
         self._trace_level = 0
         _trace_level = os.getenv("PYMIC_TRACE", 0)
@@ -60,13 +60,13 @@ class pymicConfig:
         # PYMIC_LIBRARY_PATH
         self._search_path = os.getenv("PYMIC_LIBRARY_PATH", "")
 
-        
+
 _config = pymicConfig()
 
 
 # determine the integer value for PYMIC_DEBUG
 if _config._debug_level is not None:
-    print("PYMIC: debug level set to {0}".format(_config._debug_level), 
+    print("PYMIC: debug level set to {0}".format(_config._debug_level),
           file=sys.stderr)
 
 
@@ -76,7 +76,7 @@ def _debug(level, format, *objs):
         print(msg, file=sys.stderr)
     return None
 
-    
+
 def _deprecated(func):
     def wrapper(*args):
         print("Warning: function '{0}' has been "
@@ -84,7 +84,7 @@ def _deprecated(func):
         func(*args)
     return wrapper
 
-    
+
 def _get_order(array):
     """Determine the storage order of an array and map it to its string
        representation.
@@ -102,7 +102,7 @@ _data_type_map = {
     # Python types
     int: 0,
     float: 1,
-    complex: 2, 
+    complex: 2,
 
     # Numpy dtypes
     numpy.dtype(numpy.int64): 0,
@@ -118,17 +118,17 @@ def _map_data_types(dtype):
     return _data_type_map[dtype]
 
 
-class _FakePointer:
+class _DeviceAllocation:
     """Smart pointer to store the fake pointer and perform pointer
        translation and offset computation."""
-       
+
     _stream = None
     _device_id = None
     _device_ptr = None
     _sticky = False
     _allocated = False
     _offset = 0
-       
+
     def __init__(self, stream, device, device_ptr, sticky):
         """Initialize the fake pointer with the data coming from
            OffloadStream.allocate_device_memory."""
@@ -139,20 +139,20 @@ class _FakePointer:
         self._device = device
         self._device_ptr = device_ptr
         self._sticky = sticky
-    
-    def __str__(self): 
+
+    def __str__(self):
         """Pretty print the value of this fake pointer."""
         return '0x{0:x}+{1}'.format(self._device_ptr, self._offset)
-    
+
     def __del__(self):
         if self._allocated and not self._sticky:
             self._stream.deallocate_device_memory(self)
-    
+
     def __add__(self, offset):
         """Add an offset to the fake pointer."""
         if not isinstance(offset, int):
             raise ValueError('Only int offsets allowed for fake pointers.')
-        new = _FakePointer(self._stream, self._device, self._device_ptr, 
+        new = _DeviceAllocation(self._stream, self._device, self._device_ptr,
                            self._sticky)
         new._offset = self._offset + offset
         return new
@@ -163,12 +163,12 @@ class _FakePointer:
             raise ValueError('Only int offsets allowed for fake pointers.')
         self._offset += offset
         return self
-    
+
     def __sub__(self, offset):
         """Substract an offset to the fake pointer."""
         if not isinstance(offset, int):
             raise ValueError('Only int offsets allowed for fake pointers.')
-        new = _FakePointer(self._stream, self._device, self._device_ptr, 
+        new = _DeviceAllocation(self._stream, self._device, self._device_ptr,
                            self._sticky)
         new._offset = self._offset - offset
         return new
